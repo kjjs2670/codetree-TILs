b18c60b966de78ba61446a5759287b068ed51a87
kjjs2670 <74890445+kjjs2670@users.noreply.github.com>
2024-07-16 17:35:09 +0900

    Title: 데이터 교환, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

--- 240716/데이터 교환/exchange-data.java
+++ 240716/데이터 교환/exchange-data.java
@@ -2,13 +2,20 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         int a = 5, b = 6, c = 7;
-        int temp1, temp2;
+        // int temp1, temp2;
+        int temp;
 
-        temp1 = b;
+        // temp1 = b;
+        // b = a;
+        // temp2 = c;
+        // c = temp1;
+        // a = temp2;
+
+        temp  = c;
+        c = b;
         b = a;
-        temp2 = c;
-        c = temp1;
-        a = temp2;
+        a = temp;
+
 
 
 

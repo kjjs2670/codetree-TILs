b8e1e97490876d000667df0eb7c72dff3cf65c50
kjjs2670 <74890445+kjjs2670@users.noreply.github.com>
2024-11-20 15:07:10 +0900

    Title: 좀 더 어려운 수학 점수, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241120/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int aMath = sc.nextInt();
+        int aEng = sc.nextInt();
+        int bMath = sc.nextInt();
+        int bEng = sc.nextInt();
+        if (aMath > bMath){
+            System.out.println("A");
+        }
+        else if (aMath < bMath){
+            System.out.println("B");
+        }
+        else{
+            if (aEng > bEng){
+                System.out.println("A");
+            }
+            else{
+                System.out.println("B");
+            }
+        }
+    }
+}
